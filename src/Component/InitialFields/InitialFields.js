b24e9774ad4c40c3e477cf0c0582047169b64523
Yhonny <yhonny28>
2020-03-20 13:15:49 -0600

    v6

--- src/Component/InitialFields/InitialFields.js
+++ src/Component/InitialFields/InitialFields.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './InitialFields.css';
+import { render } from '@testing-library/react';
 
-function InitialFields(props) {
-    return (
-        <div>
-            <div>
-                <input type="text" placeholder='First Name' className='Generic' />
-                <input type="text" placeholder='Last Name' className='Generic' /> <input type="text" placeholder='Account ID' className='Generic' />
-            </div>
-        </div>
-    );
+class InitialFields extends React.Component {
+
+    constructor(pros) {
+        super(pros);
+        this.state = { firstName: '', lastName: '', id: '' };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({ [name]: value });
+
+    }
+
+
+
+    render() {
+        const { firstName, lastName, id } = this.state
+        return (
+            <form>
+                <div className='Spacing'>
+                    <input className='Generic' placeholder='First Name' type="text" name='firstName' value={this.state.firstName} onChange={this.handleChange} />
+                    <input className='Generic' placeholder='Last Name' type="text" name='lastName' value={this.state.lastName} onChange={this.handleChange} />
+                    <input className='Generic' placeholder='Account ID' type="number" number='id' value={this.state.id} onChange={this.handleChange} />
+                </div>
+                <button className='Submit' type="submit">Submit</button>
+            </form >
+        );
+    }
 }
 
 export default InitialFields;
