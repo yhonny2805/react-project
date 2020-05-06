0ea01d92aad5fbb708c8d243ab14ae499fa7d3f8
Yhonny <yhonny28>
2020-05-05 19:14:49 -0600

    new-version

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import * as serviceWorker from './serviceWorker';
 //import Footer from "./Component/Footer/Footer";
 
 import Greetings from './Component/Greetings/Greetings';
-//import Loginpage from './Component/Loginpage/Loginpage';
-//import Initialfields from './Component/Initialfields/Initialfields';
 //import UseridList from './Component/UseridList/UseridList';
 //import Navigationmenu from './Component/Navigationmenu/Navigationmenu';
-//import Events from './Component/Events/Events';
 //import Initloginpage from './Component/Initloginpage/Initloginpage';
 //import EmployeeList from './Component/EmployeeList/EmployeeList';
 //import History from './Component/History/History';
@@ -19,11 +16,6 @@ import Greetings from './Component/Greetings/Greetings';
 //import HistoryH from './Component/HistoryH/HistoryH';
 import LoginControl from './Component/LoginControl/LoginControl';
 
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
 ///////////////////////////////////////////////////////////////////////
@@ -41,12 +33,6 @@ ReactDOM.render(<LoginControl />, document.getElementById('root'));
 
 //** Navigation Menu **
 //ReactDOM.render(<Navigationmenu />, document.getElementById('root'));
-
-//** Initialfields **
-//RectDOM.render(<Initialfields />, document.getElementById('root'));
-
-//** Loginpage **
-//ReactDOM.render(<Loginpage />, document.getElementById('root'));
 
 // ** Lista **
 //ReactDOM.render(<UseridList />, document.getElementById('root'));
